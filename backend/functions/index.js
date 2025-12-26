3258241c32c83c2dec28f2b0ef2c1646ad9ab8ee
mohitUpraity <mohitupraity123@gmail.com>
2025-12-26 18:12:12 +0530

    completed till now

--- backend/functions/index.js
+++
@@ -1,7 +0,0 @@
-// Cloud Functions temporarily disabled.
-// You previously added callable functions for approve/reject. To avoid running backend logic
-// while you're iterating locally, this file is intentionally left as a placeholder.
-
-// When ready to re-enable, replace this content with your function implementations and re-deploy.
-
-module.exports = {};
